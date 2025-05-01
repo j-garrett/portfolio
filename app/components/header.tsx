07e4febec3ccb4d8d6baea540e15e861ba90f582
Jon Garrett <jonathan.garrett@gmail.com>
2025-05-01 16:10:49 -0600

    Update Header and main layout

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,11 +1,21 @@
 const Header = () => {
   return (
-    <div className="bg-blue-500 grid grid-flow-col grid-rows-3 gap-4">
-      <div className="bg-pink-500 row-span-3">
+    <div className="grid grid-cols-4 gap-4">
+      <div className="col-span-3 self-center">
         <h1 className="text-6xl">Jon Garrett</h1>
       </div>
-      <div className="bg-teal-500 col-span-2">Test right side1</div>
-      <div className="bg-teal-500 col-span-2">Test right side2</div>
+      <address className="grid grid-row-3 gap-2 col-span-1 justify-items-end">
+        <div className="content-center">
+          <a href="mailto:jon@j-garrett.com">Email</a>
+        </div>
+        <div className="content-center">
+          <a href="https://www.linkedin.com/in/jon-garrett/">LinkedIn</a>
+        </div>
+        <div className="content-center">
+          <a href="https://github.com/j-garrett">GitHub</a>
+        </div>
+      </address>
+      <hr className="col-span-4" />
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,26 +4,30 @@ import Header from './components/header'
 
 export default function Page() {
   return (
-    <div className="columns-1">
-      <Header />
-      <JobHistory />
-      <div className="columns-3">
-        <div>WaterColors:</div>
-        <div>
-          <Image
-            alt="Profile"
-            height={291}
-            priority
-            sizes="100vw"
-            src="/king-oliver.jpg"
-            style={{
-              height: 'auto',
-              width: '100%',
-            }}
-            width={203}
-          />
+    <div className="grid grid-cols-9 gap-6">
+      <div className="col-span-1 bg-cyan-200"></div>
+      <div className="col-span-7">
+        <Header />
+        <JobHistory />
+        <div className="columns-3">
+          <div>WaterColors:</div>
+          <div>
+            <Image
+              alt="Profile"
+              height={291}
+              priority
+              sizes="100vw"
+              src="/king-oliver.jpg"
+              style={{
+                height: 'auto',
+                width: '100%',
+              }}
+              width={203}
+            />
+          </div>
         </div>
       </div>
+      <div className="col-span-1 bg-cyan-200"></div>
     </div>
   )
 }
