533f9a80407dcf5f7cb0e953cdd1516f8f361f83
Jon Garrett <jonathan.garrett@gmail.com>
2025-05-01 11:57:00 -0600

    Lint/Style config w/ pre-commit hooks
    Use next.js eslint settings.
    
    Setup prettier config and include in eslint config.
    
    Add lint-staged and husky packages and their configs.
    
    Set node and pnpm engines in package.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,38 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig } from 'eslint/config'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+  recommendedConfig: js.configs.recommended,
+})
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ['js/recommended'],
   },
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     languageOptions: { globals: globals.browser },
   },
   tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  ...compat.config({
+    extends: [
+      'eslint:recommended',
+      'next',
+      'next/core-web-vitals',
+      'next/typescript',
+      'prettier',
+    ],
+  }),
   {
     rules: {
-      "react/react-in-jsx-scope": 0,
-      "react/jsx-uses-react": 0,
+      'react/react-in-jsx-scope': 0,
+      'react/jsx-uses-react': 0,
     },
   },
-]);
+])
