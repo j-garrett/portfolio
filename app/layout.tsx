3361a126ff308ed4205672df8e885e010b23ebe2
Jon Garrett <jonathan.garrett@gmail.com>
2025-05-07 20:27:47 -0600

    add fonts, sidebar, stub routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 // These styles apply to every route in the application
 import './globals.css'
+import Sidebar from './components/sidebar'
+import Header from './components/header'
 
 export const metadata: Metadata = {
   description:
@@ -15,7 +17,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="flex text-cyan-900">
+        <Sidebar />
+        <div className="grid grid-cols-9 gap-6">
+          <div className="col-span-1 bg-cyan-200"></div>
+          <div className="col-span-7">
+            <div className="pl-10">
+              <Header />
+            </div>
+            {children}
+          </div>
+          <div className="col-span-1 bg-cyan-200"></div>
+        </div>
+      </body>
     </html>
   )
 }
