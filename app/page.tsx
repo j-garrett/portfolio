efb250c8d6b1b3629064c51ec4d5500478c51fe5
Jon Garrett <jonathan.garrett@gmail.com>
2025-05-12 16:47:04 -0600

    clean up root layout and page

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,12 @@
-import Image from 'next/image'
-import JobHistory from './components/job-history'
-// import Header from './components/header'
+import MainContainer from './components/main-container'
 
 export default function Page() {
   return (
-    // <div className="grid grid-cols-9 gap-6">
-    //   <div className="col-span-1 bg-cyan-200"></div>
-    //   <div className="col-span-7">
-    //     <div className="pl-10">
-    //       <Header />
-    //     </div>
     <div>
-      <JobHistory />
-      <div className="border-t-1 border-t-cyan-500">
-        <div className="grid grid-cols-3">
-          <div className="col-start-2 pt-5">
-            <Image
-              alt="Profile"
-              height={291}
-              priority
-              sizes="100vw"
-              src="/king-oliver.jpg"
-              style={{
-                height: 'auto',
-                width: '100%',
-              }}
-              width={203}
-            />
-          </div>
-        </div>
-      </div>
+      <h2 className="text-4xl pl-5">Projects</h2>
+      <MainContainer>
+        <p>it&apos;s all bout mememeAbout!</p>
+      </MainContainer>
     </div>
-    //   </div>
-    //   <div className="col-span-1 bg-cyan-200"></div>
-    // </div>
   )
 }
