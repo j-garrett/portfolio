1d00210b18a7d688a475aa8fd6861f01279e2113
Jon Garrett <jonathan.garrett@gmail.com>
2025-05-12 18:59:34 -0600

    add section/slug for scavenger-hunt and portfolio blogs

---
+++ app/blog/scavenger-hunt/[slug]/page.tsx
@@ -0,0 +1,11 @@
+import BlogPostPage from '../../components/blog-post-page'
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>
+}) {
+  const { slug } = await params
+
+  return <BlogPostPage slug={slug} subDirectory="scavenger-hunt" />
+}
