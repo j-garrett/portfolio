648a5894cfdde4f3e820f220ca5589d2a86e8a68
Jon Garrett <jonathan.garrett@gmail.com>
2025-05-13 11:23:50 -0600

    use flex for main-container

--- app/components/main-container.tsx
+++ app/components/main-container.tsx
@@ -7,7 +7,7 @@ export default function MainContainer({
 }) {
   return (
     <div className="flex flex-grow border-t-1 border-t-cyan-200 min-h-lvh h-auto">
-      <div className="block flex-grow justify-between col-span-9">
+      <div className="flex flex-grow justify-between col-span-9">
         {children}
       </div>
     </div>
